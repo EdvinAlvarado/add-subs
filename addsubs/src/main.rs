4b0dfb5d3762af8c419762898afb599d2ce33d04
EdvinAlvarado <ealvarado301@gmail.com>
2023-07-19 23:17:25 -0500

    v1.2 fixed sorting removed MisMatchError due to false positives print video files to show progress. Will now print output of mkvmerge for traceability reorganize some code

--- addsubs/src/main.rs
+++ addsubs/src/main.rs
@@ -1,7 +1,6 @@
 use std::path::Path;
 use std::error::Error;
-use std::fs;
-use std::fmt;
+use std::{fs, fmt, str};
 use std::collections::HashMap;
 use std::process::Command;
 
@@ -25,7 +24,7 @@ impl fmt::Display for ProgramError {
 }
 
 
-fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result<(), Box<dyn Error>> {
+fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result<Vec<Vec<u8>>, Box<dyn Error>> {
 
     // ISO 639.2
     let langs = HashMap::from([
@@ -43,8 +42,10 @@ fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result
         let f = file.unwrap().file_name().into_string().unwrap();
         if f.contains(videoformat) {videofiles.push(f);}
         else if f.contains(subformat) {subfiles.push(f);}
-    }    
-    if videofiles.len() == subfiles.len() {return Err(Box::new(ProgramError::MismatchError));}
+    }
+    videofiles.sort();
+    subfiles.sort();
+    //if videofiles.len() == subfiles.len() {return Err(Box::new(ProgramError::MismatchError));}
 
     // Check
     println!("Joining sub files to these video files.");
@@ -58,25 +59,39 @@ fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result
     std::io::stdin().read_line(&mut answer)?;
     if answer.contains("n") {return Err(Box::new(ProgramError::ExitError));}
 
+
     // Run commands
+    let mut res = Vec::new();
     Command::new("mkdir").arg("output").output().expect("Could not create output folder.");
     for (s,v) in file_iter {
-        Command::new("mkvmerge").args([
-            "-o",
-            &format!("'output/{}'", v),
-            &format!("'{}'", v),
-            "--language",
-            &format!("0:{}", lang),
-            "--track-name",
-            &format!("0:{}", langs.get(lang).unwrap()),
-            &format!("'{}'", s)
-        ]).output().expect("mkvmerge command failed to run. Is mkvmerge installed?");
+        println!("{}", v);
+
+        let args = [
+        "-o",
+        &format!("{}/output/{}",dir.display(), v),
+        &format!("{}", v),
+        "--language",
+        &format!("0:{}", lang),
+        "--track-name",
+        &format!("0:{}", langs.get(lang).unwrap()),
+        &format!("{}", s)
+        ];
+        
+        let output = Command::new("mkvmerge").args(args).output().expect("mkvmerge command failed to run. Is mkvmerge installed?");
+        res.push(output.stdout);
     }
-    
-    Ok(())
+ 
+    Ok(res)
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    addsubs(Path::new(&args[1]), &args[2], &args[3], &args[4]).unwrap()
+    let res = addsubs(Path::new(&args[1]), &args[2], &args[3], &args[4]).unwrap();
+    for buf in res {
+        let rs = match str::from_utf8(&buf) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        println!("{}", rs);
+    }
 }
