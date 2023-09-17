a467a5960cb58459739200734ff076ff82923e0c
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-17 18:40:32 -0500

    change dir from &Path to AsRef<Path>

--- addsubs/src/main.rs
+++ addsubs/src/main.rs
@@ -26,7 +26,7 @@ impl fmt::Display for ProgramError {
 }
 
 
-fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result<Vec<Vec<u8>>, Box<dyn Error>> {
+fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &str) -> Result<Vec<Vec<u8>>, Box<dyn Error>> {
 
     // ISO 639.2
     let langs = HashMap::from([
@@ -40,7 +40,7 @@ fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result
     // Create list of files.
     let mut videofiles = Vec::new();
     let mut subfiles = Vec::new();
-    for file in fs::read_dir(dir)? {
+    for file in fs::read_dir(dir.as_ref())? {
         let f = file.unwrap().file_name().into_string().unwrap();
         if f.contains(videoformat) {videofiles.push(f);}
         else if f.contains(subformat) {subfiles.push(f);}
@@ -70,7 +70,7 @@ fn addsubs(dir: &Path, videoformat: &str, subformat: &str, lang: &str) -> Result
 
         let args = [
         "-o",
-        &format!("{}/output/{}",dir.display(), v),
+        &format!("{}/output/{}",dir.as_ref().display(), v),
         &format!("{}", v),
         "--language",
         &format!("0:{}", lang),
@@ -97,7 +97,7 @@ fn main() {
 		panic!("{}", ProgramError::NoArgumentError);
 	}
 
-    let res = addsubs(Path::new(&args[1]), &args[2], &args[3], &args[4]).unwrap();
+    let res = addsubs(&args[1], &args[2], &args[3], &args[4]).unwrap();
     for buf in res {
         let rs = match str::from_utf8(&buf) {
             Ok(v) => v,
