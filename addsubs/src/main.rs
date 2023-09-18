d29ad909a6ce8d374bee27a328caefaccdbcda01
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-17 23:56:28 -0500

    v1.1.4 Better error handling and shows error message in cli. changes videofiles and subfiles from Rc<str> to Box<str> init res with capacity

--- addsubs/src/main.rs
+++ addsubs/src/main.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 use std::{fs, fmt, str};
 use std::collections::HashMap;
 use std::process::Command;
-
+use main_error::MainError;
 
 #[derive(Debug)]
 pub enum ProgramError {
@@ -12,8 +12,14 @@ pub enum ProgramError {
     LangError(Box<str>),
     ExitError,
 	NoArgumentError,
-	InputError(std::io::Error)
+	InputError(std::io::Error),
 }
+
+static NOARGUMENT: &str = "The program requires 4 arguments to be provide:
+\t 1. directory
+\t 2. video file format (e.g. mkv)
+\t 3. sub file format (e.g. srt)
+\t 4. supported language code (e.g. jpn)";
 
 impl Error for ProgramError {}
 impl fmt::Display for ProgramError {
@@ -22,12 +28,11 @@ impl fmt::Display for ProgramError {
             ProgramError::MismatchError => write!(f, "Not the same amount of video and sub files."),
             ProgramError::LangError(lang) => write!(f, "{} language not supported.", *lang),
             ProgramError::ExitError => write!(f, "User cancelled."),
-			ProgramError::NoArgumentError => write!(f, "Not enough arguments provided."),
 			ProgramError::InputError(e) => write!(f, "Input error: {}", e),
+			ProgramError::NoArgumentError => write!(f,"{}", NOARGUMENT),
         }
     }
 }
-
 impl From<std::io::Error> for ProgramError {
 	fn from(err: std::io::Error) -> ProgramError {
 		ProgramError::InputError(err)
@@ -50,7 +55,7 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
     let mut videofiles = Vec::new();
     let mut subfiles = Vec::new();
     for file in fs::read_dir(dir.as_ref())? {
-        let f: Rc<str> = file?.file_name().to_string_lossy().into();
+        let f: Box<str> = file?.file_name().to_string_lossy().into();
         if f.contains(videoformat) {videofiles.push(f);}
         else if f.contains(subformat) {subfiles.push(f);}
     }
@@ -72,11 +77,12 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
 
 
     // Run commands
-    let mut res = Vec::new();
-    Command::new("mkdir").arg("output").output().expect("Could not create output folder.");
+    let mut res = Vec::with_capacity(videofiles.len());
+    Command::new("mkdir").arg("output").output()?;
     for (s,v) in file_iter {
         println!("{}", v);
-
+		
+		// mkvmerge -o [dir]/output/[videofile] [videofile] --language 0:jpn --track-name 0:Japanese [subfile]
         let args = [
         "-o",
         &format!("{}/output/{}",dir.as_ref().display(), v),
@@ -88,23 +94,16 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
         &format!("{}", s)
         ];
         
-        let output = Command::new("mkvmerge").args(args).output().expect("mkvmerge command failed to run. Is mkvmerge installed?");
+        let output = Command::new("mkvmerge").args(args).output()?;
         res.push(output.stdout);
     }
  
     Ok(res)
 }
 
-fn main() -> Result<(), ProgramError> {
+fn main() -> Result<(), MainError> {
     let args: Rc<[String]> = std::env::args().collect();
-	if args.len() < 5 {
-		println!("The program requires 4 arguments to be provided:");
-		println!("\t1. directory");
-		println!("\t2. video file format (e.g. mkv)");
-		println!("\t3. sub file format (e.g. srt)");
-		println!("\t4. supported language code (e.g. jpn)");
-		return Err(ProgramError::NoArgumentError);
-	}
+	(args.len() == 5).then_some(0).ok_or(ProgramError::NoArgumentError)?;
 
     for res in addsubs(&args[1], &args[2], &args[3], &args[4])?.iter() {
 		let rs = String::from_utf8_lossy(res);
