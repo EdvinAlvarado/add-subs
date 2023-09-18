e1b994a059d2470bcdafc27bff09fcc4e83d9015
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-17 19:49:51 -0500

    v1.1.2 implemented Rc where sensible Changed addsubs from Box<dyn Error> to ProgramError Cleaned up

--- addsubs/src/main.rs
+++ addsubs/src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::error::Error;
+use std::rc::Rc;
 use std::{fs, fmt, str};
 use std::collections::HashMap;
 use std::process::Command;
@@ -25,8 +26,9 @@ impl fmt::Display for ProgramError {
     }
 }
 
+type Stdout = Vec<u8>;
 
-fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &str) -> Result<Vec<Vec<u8>>, Box<dyn Error>> {
+fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &str) -> Result<Vec<Stdout>, ProgramError> {
 
     // ISO 639.2
     let langs = HashMap::from([
@@ -35,13 +37,13 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
                               ("spa", "Spanish"),
                               ("und", "Undetermined"),
     ]);
-    if !langs.contains_key(lang) {return Err(Box::new(ProgramError::LangError));}
+    if !langs.contains_key(lang) {return Err(ProgramError::LangError);}
 
     // Create list of files.
     let mut videofiles = Vec::new();
     let mut subfiles = Vec::new();
-    for file in fs::read_dir(dir.as_ref())? {
-        let f = file.unwrap().file_name().into_string().unwrap();
+    for file in fs::read_dir(dir.as_ref()).unwrap() {
+        let f: Rc<str> = file.unwrap().file_name().to_str().unwrap().into();
         if f.contains(videoformat) {videofiles.push(f);}
         else if f.contains(subformat) {subfiles.push(f);}
     }
@@ -58,8 +60,8 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
 
     println!("Are these pairs correct? (Y/n): ");
     let mut answer = String::new();
-    std::io::stdin().read_line(&mut answer)?;
-    if answer.contains("n") {return Err(Box::new(ProgramError::ExitError));}
+    std::io::stdin().read_line(&mut answer).unwrap();
+    if answer.contains("n") {return Err(ProgramError::ExitError);}
 
 
     // Run commands
@@ -87,7 +89,7 @@ fn addsubs<P: AsRef<Path>>(dir: P, videoformat: &str, subformat: &str, lang: &st
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Rc<[String]> = std::env::args().collect();
 	if args.len() < 5 {
 		println!("The program requires 4 arguments to be provided:");
 		println!("\t1. directory");
@@ -97,12 +99,8 @@ fn main() {
 		panic!("{}", ProgramError::NoArgumentError);
 	}
 
-    let res = addsubs(&args[1], &args[2], &args[3], &args[4]).unwrap();
-    for buf in res {
-        let rs = match str::from_utf8(&buf) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-        println!("{}", rs);
-    }
+    for res in addsubs(&args[1], &args[2], &args[3], &args[4]).unwrap().iter() {
+		let rs = str::from_utf8(res).unwrap();
+		println!("{}", rs);
+	}
 }
